7ef4dda0032b4cb0afebc344991c3212ffca8793
Daniel Downey <x14728599@student.ncirl.ie>
2016-12-06 21:19:37 +0000

    Added third query

---
+++ CA Query 3.sql
@@ -0,0 +1,10 @@
+#Which staff members have rented out Spanish movies the greatest number of times
+
+select dim_staff.staff_first_name, count(fact_rental.film_key) as 'Number of Spanish films rented out'
+from dim_staff
+inner join fact_rental
+on dim_staff.staff_key = fact_rental.staff_key
+inner join dim_film
+on fact_rental.film_key = dim_film.film_key
+where dim_film.film_language = 'Spanish'
+group by dim_staff.staff_first_name desc;
