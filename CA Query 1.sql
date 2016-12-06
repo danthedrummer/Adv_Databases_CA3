7887a56dba073ca7f8134a1cdca7816983c7e505
Daniel Downey <x14728599@student.ncirl.ie>
2016-12-06 18:51:36 +0000

    Added first query

---
+++ CA Query 1.sql
@@ -0,0 +1,7 @@
+#For each store list the number of different customers who rent films
+
+select dim_store.store_id, count(dim_customer.customer_id) 
+from dim_store
+inner join dim_customer
+on dim_store.store_id = dim_customer.customer_store_id
+group by store_id;
