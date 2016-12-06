b44395d6fa16169d851f5b7dc374f342ff559cd5
Daniel Downey <x14728599@student.ncirl.ie>
2016-12-06 18:58:03 +0000

    Added first query

--- CA Query 1.sql
+++ CA Query 1.sql
@@ -4,5 +4,6 @@ select dim_store.store_id, count(dim_customer.customer_id)
 from dim_store
 inner join dim_customer
 on dim_store.store_id = dim_customer.customer_store_id
-where dim_customer.customer_active = 1
+#Makes sure the customer is an active customer
+where dim_customer.customer_active = 1	
 group by store_id;
