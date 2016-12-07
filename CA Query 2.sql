7a654deb2a3944d4f86cf99e638d21ab8a88871d
Daniel Downey <x14728599@student.ncirl.ie>
2016-12-07 09:53:08 +0000

    Fixed date error

--- CA Query 2.sql
+++ CA Query 2.sql
@@ -1,10 +1,11 @@
-#List the 10 most popular movies rented in May 2015
+#List the 10 most popular movies rented in May 2005
 
 select 
-	dim_film.film_title as 'Popular May 2015', 
+	dim_film.film_title as 'Popular May 2005', 
     count(fact_rental.film_key) as 'Times Rented'
-from dim_film
+from dim_film 
 inner join fact_rental
 	on dim_film.film_key = fact_rental.film_key
+where fact_rental.rental_date between '2005-05-01 00:00:00' and '2005-06-01 00:00:00'
 group by dim_film.film_key order by count(fact_rental.film_key) desc
 limit 0, 9;
